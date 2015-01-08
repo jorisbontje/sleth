8fcbe4e9047a0e566c5c9ad76f37102d43a88519
Joris Bontje <jorisbontje@gmail.com>
2015-01-08 12:39:40 +0100

    clarify test case

--- test/test_arrays.py
+++ test/test_arrays.py
@@ -10,12 +10,19 @@ def arr3():
 def pass(rnd):
     return(self.arr3(outsz=3), 3)
 
-def test():
-    arr = self.ta()
-    return(arr[1])  # how to get 4 to be returned?
-
 def ta():
     return([3,4,5]:a)
+
+def test():
+    arr = self.ta(outsz=3)
+    return(arr[1])  # how to get 4 to be returned?
+
+def ta2():
+    return([3,4,5], 3)
+
+def test2():
+    arr = self.ta2(outsz=3)
+    return(arr[1])  # this does return 4 however
 """
 
     def setup_class(cls):
@@ -35,5 +42,8 @@ def ta():
     def test_arr3_pass(self):
         assert self._call(1) == [1, 2, 3]
 
-    def test_test_ta(self):
-        assert self._call(2) == [4]
+    def test_ta(self):
+        assert self._call(3) == [4]
+
+    def test_ta2(self):
+        assert self._call(5) == [4]
