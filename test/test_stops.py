3e2d8a33b21c02a53e18749f779b9ecf50cb366c
Joris Bontje <jorisbontje@gmail.com>
2015-01-15 10:47:29 +0100

    use tester.abi_contract

--- test/test_stops.py
+++ test/test_stops.py
@@ -16,13 +16,13 @@ def get_stops(rnd):
         i += 1
     return(stops, REEL_COUNT)
 
-def pass(rnd):
+def pass_(rnd):
     return(self.get_stops(rnd, outsz=REEL_COUNT), REEL_COUNT)
 """
 
     def setup_class(cls):
         cls.s = tester.state()
-        cls.c = cls.s.contract(cls.CONTRACT)
+        cls.c = cls.s.abi_contract(cls.CONTRACT)
         cls.snapshot = cls.s.snapshot()
 
     def setup_method(self, method):
@@ -35,9 +35,9 @@ def pass(rnd):
         return self.s.send(tester.k0, self.c, 0, funid=1, abi=[rnd])
 
     def test_get_stops(self):
-        assert self._get_stops(23888) == [16, 10, 23]
-        assert self._get_stops(1606) == [6, 18, 1]
-        assert self._get_stops(30464) == [0, 24, 29]
+        assert self.c.get_stops(23888) == [16, 10, 23]
+        assert self.c.get_stops(1606) == [6, 18, 1]
+        assert self.c.get_stops(30464) == [0, 24, 29]
 
     def test_pass(self):
-        assert self._pass(23888) == [16, 10, 23]
+        assert self.c.pass_(23888) == [16, 10, 23]
