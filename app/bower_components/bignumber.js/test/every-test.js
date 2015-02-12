8e36696bd058532081343fa983a8b8e259beca73
Joris Bontje <jorisbontje@gmail.com>
2015-02-12 08:05:23 +0100

    Update 2015-02-12T07:05:00.236Z

---
+++ app/bower_components/bignumber.js/test/every-test.js
@@ -0,0 +1,43 @@
+var arr,
+    passed = 0,
+    total = 0,
+    start = +new Date();
+
+console.log( '\n STARTING TESTS...\n' );
+
+[
+  'abs',
+  'base-in',
+  'base-out',
+  'ceil',
+  'cmp',
+  'config',
+  'dp',
+  'div',
+  'divToInt',
+  'floor',
+  'minus',
+  'mod',
+  'neg',
+  'others',
+  'plus',
+  'pow',
+  'round',
+  'sqrt',
+  'times',
+  'toExponential',
+  'toFixed',
+  'toFormat',
+  'toFraction',
+  'toNumber',
+  'toPrecision',
+  'toString'
+]
+.forEach( function (method) {
+    arr = require('./' + method);
+    passed += arr[0];
+    total += arr[1];
+});
+
+console.log( '\n IN TOTAL: ' + passed + ' of ' + total + ' tests passed in ' +
+    ( (+new Date() - start) / 1000 ) + ' secs.\n' );
