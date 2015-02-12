c79ed33f06e5aabf251f2cb85d9e4475cae974c9
Joris Bontje <jorisbontje@gmail.com>
2015-02-12 08:06:02 +0100

    fix for stdout maxBuffer exceeded

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,6 @@ var deploy      = require('gulp-gh-pages');
  * Push build to gh-pages
  */
 gulp.task('deploy', function () {
-  return gulp.src("./frontend/**/*")
-    .pipe(deploy())
+  return gulp.src(['./frontend/**/*', '!./frontend/node_modules/**'])
+    .pipe(deploy({cacheDir: '../sleth-gh-pages/'}))
 });
