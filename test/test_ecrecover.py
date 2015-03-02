4e87cd140dc7bfdbaa87c2fa023c6c006bf89c1e
Joris Bontje <jorisbontje@gmail.com>
2015-03-02 08:25:48 +0100

    demo how to use ecrecover

---
+++ test/test_ecrecover.py
@@ -0,0 +1,33 @@
+import bitcoin as b
+from pyethereum import tester, utils
+
+
+class TestECRecover(object):
+
+    CONTRACT = """
+
+def test_ecrecover(h, v, r, s):
+    return(ecrecover(h, v, r, s))
+"""
+
+    def setup_class(cls):
+        cls.s = tester.state()
+        cls.c = cls.s.abi_contract(cls.CONTRACT)
+        cls.snapshot = cls.s.snapshot()
+
+    def setup_method(self, method):
+        self.s.revert(self.snapshot)
+
+    def test_ecrecover(self):
+        priv = b.sha256('some big long brainwallet password')
+        pub = b.privtopub(priv)
+
+        msghash = b.sha256('the quick brown fox jumps over the lazy dog')
+        V, R, S = b.ecdsa_raw_sign(msghash, priv)
+        assert b.ecdsa_raw_verify(msghash, (V, R, S), pub)
+
+        addr = utils.big_endian_to_int(utils.sha3(b.encode_pubkey(pub, 'bin')[1:])[12:])
+        assert int(utils.privtoaddr(priv), 16) == addr
+
+        result = self.c.test_ecrecover(utils.big_endian_to_int(msghash.decode('hex')), V, R, S)
+        assert result == addr
